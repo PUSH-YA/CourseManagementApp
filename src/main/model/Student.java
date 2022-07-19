5f185081c62edc94f38e1ca678c0e3f8f54a26e3
PUSH-YA <pushya22.jain@gmail.com>
2022-07-18 18:16:18 -0700

    Fixed a typo and removed a useless import

--- src/main/model/Student.java
+++ src/main/model/Student.java
@@ -4,14 +4,13 @@ import jdk.nashorn.internal.ir.Assignment;
 
 import java.util.Date;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 
 public class Student {
 
     private String name;
     private List<Course> listOfCourses;
-    private HashMap<Date, Object> shedule;
+    private HashMap<Date, Object> schedule;
 
     public void reminder(){
 
