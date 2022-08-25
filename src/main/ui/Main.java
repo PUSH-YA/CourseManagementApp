249a56b219aae0731c2aa809b42a13db947326d6
PUSH-YA <91928008+PUSH-YA@users.noreply.github.com>
2022-08-25 00:17:54 -0700

    Implemented the backend and the frontend of the app

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -10,8 +10,5 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         AskNameUI name =  new AskNameUI();
-//        JsonReader reader = new JsonReader("./data/pushya.json");
-//        Student s = reader.read();
-//        EditHomeWorkUI ui = new EditHomeWorkUI(s);
     }
 }
